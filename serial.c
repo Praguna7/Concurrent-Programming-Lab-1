a727bdc7f1a9e20d643ebbf5dc8523c8c082811d
Praguna Chandrasekara <praguna7@gmail.com>
2024-10-10 15:06:45 +0530

    serial program updated

--- serial.c
+++ serial.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 #include "linked_list.h"
 #include "serial.h"
-#define MAX_VALUE 65535  // 2^16 - 1
+#include <sys/time.h>
 
-int test_serial(struct list_node_s ** head, int n, int m, float m_insert_frac, double m_member_frac, double m_delete_frac){
 
-    populate_list(head,n);
+int test_serial(int n, int m, float m_insert_frac, float m_member_frac, float m_delete_frac){
+
+    struct list_node_s* head = NULL;
+    populate_list(&head,n);
 
     srand(time(0)); // Use current time as random seed
 
-    int ins_ops_limit = (int) (m * m_insert_frac);
-    int mem_ops_limit = (int) (m * m_member_frac);
-    int del_ops_limit = (int) (m * m_delete_frac);
+    int ins_ops_limit = (int) ceil(m * m_insert_frac);
+    int mem_ops_limit = (int) ceil(m * m_member_frac);
+    int del_ops_limit = (int) ceil(m * m_delete_frac);
 
     int ins_ops_count = 0;
     int mem_ops_count = 0;
@@ -22,44 +25,38 @@ int test_serial(struct list_node_s ** head, int n, int m, float m_insert_frac, d
     int operation;
     int rand_val;
 
+    struct timeval start;
+    struct timeval stop;
+    gettimeofday(&start, NULL); 
     while(total_ops_count<m){
+        // printf("tot ops %i",total_ops_count);
         operation = rand()%3;
         rand_val = rand()%MAX_VALUE;
 
         if(operation==0 && ins_ops_count<ins_ops_limit){
-            Insert(rand_val,head);
+            Insert(rand_val,&head);
             ins_ops_count++;
             total_ops_count++;
         }
         else if(operation==1 && mem_ops_count<mem_ops_limit){
-            Member(rand_val,*head);
+            Member(rand_val,head);
             mem_ops_count++;
             total_ops_count++;
             
         }
         else if (operation==2 && del_ops_count<del_ops_limit){
-            Delete(rand_val,head);
+            Delete(rand_val,&head);
             del_ops_count++;
             total_ops_count++;
         }
     }
+
+    gettimeofday(&stop, NULL); 
+    unsigned long time = (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec;
+    // printf("Serial program took %lu us\n", time); 
+
+    return time;
     
 };
 
-void populate_list(struct list_node_s ** head, int n){
-    int count = 0;
-    while(count<n){
-        int value = rand() % MAX_VALUE;
-        if(Insert(value,head)){
-            count++;
-        }
-    }
-};
 
-int main () {
-    struct list_node_s* head = NULL;
-    test_serial(&head,50,1000,0.9,0.05,0.05);
-    PrintList(head);
-    
-    return 0;
-};
--- serial.h
+++ serial.h
@@ -1,7 +1,5 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-int test_serial(struct list_node_s ** head, int n, int m, float m_insert_frac, double m_member_frac, double m_delete_frac);
-void populate_list(struct list_node_s ** head, int n);
-
+int test_serial(int n, int m, float m_insert_frac, float m_member_frac, float m_delete_frac);
 #endif
